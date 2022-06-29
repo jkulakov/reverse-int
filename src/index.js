152449ab181507f6134b946f3ab829dde63e5fb3
Ivan Kulakov <ivankulakov@MacBook-Pro-Ivan.local>
2022-06-30 00:25:58 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  // тест
-}
+  let result = '';
+  n = String(n);
+  
+  if(n[0] === '-') n = n.slice(1, n.length);
+  
+  result = n.split('').reverse().join('');
+  return Number(result);
+};
